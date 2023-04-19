ba946c89db90d18e5f2ff0c740b9f20d4e41a193
Larry Joy <ljoy@microsoft.com>
2023-04-19 15:19:32 -0700

    Create index.ts
    group modules so npm package can have single entry point

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * as uprove from "./uprove.js";
+export * as serialization from "./serialization.js";
+export * as upjf from "./upjf.js";
+export * as utils from "./utils.js";
+export { IssuerParamsJWK } from "./upjf.js";
